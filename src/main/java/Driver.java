b29d04bd5af40744ab30fa77deab75e7d2b85fe0
rizal <fahrizalhilmi2002@gmail.com>
2023-05-20 22:27:01 +0700

    Update Driver.java

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -16,6 +16,25 @@ public class Driver {
 		
 		System.out.println("Current  Count:" + counter.getCount());
 		
+		counter.increment();
+		
+		System.out.println("Current Count:" + counter.getCount());
+		
+		counter.decrement();
+		
+		System.out.println("Current  Count:" + counter.getCount());counter.increment();
+		
+		System.out.println("Current Count:" + counter.getCount());
+		
+		counter.decrement();
+		
+		System.out.println("Current  Count:" + counter.getCount());counter.increment();
+		
+		System.out.println("Current Count:" + counter.getCount());
+		
+		counter.decrement();
+		
+		System.out.println("Current  Count:" + counter.getCount());
 	}
 
 }
